2687b43cd6d390987efb800a1f50378a50fd5b10
Dave <dave@webdave.de>
2020-03-10 00:28:27 +0100

    new post

---
+++ scripts/getfiles.js
@@ -0,0 +1,63 @@
+const directory = "./";
+const fs = require("fs");
+const postsArray = [];
+let processed = 0;
+
+const posts = fs.readdirSync(directory);
+let popstCount = posts.filter(name => name.indexOf(".md") !== -1).length;
+posts.sort((a, b) => {
+  return (
+    fs.statSync(directory + a).mtime.getTime() -
+    fs.statSync(directory + b).mtime.getTime()
+  );
+});
+
+// posts.forEach(p => {
+//   console.log(fs.statSync(directory + p).mtime.getTime());
+// });
+
+posts
+  .filter(name => name.indexOf(".md") !== -1)
+  .forEach(post => {
+    let postObj = {
+      url: "",
+      name: "",
+      title: "",
+      preview: "",
+      timestamp: ""
+    };
+    postObj.url = `${post}`;
+    postObj.name = post.replace(".md", "");
+    postObj.timestamp = fs.statSync(directory + post).mtime.getTime();
+    // console.log(fs.statSync(directory + post).mtime.getTime());
+    fs.readFile(`${directory}${post}`, "utf8", function(err, postcontent) {
+      processed++;
+      if (err) {
+        return console.log(err);
+      }
+      let preview = postcontent.substr(0, 200);
+
+      posts[popstCount - 1];
+
+      postObj.preview = postcontent.substr(0, 200);
+      let title = postcontent
+        .substr(0, postcontent.indexOf("</strong>"))
+        .replace("<strong>", "");
+      postObj.title = title;
+      postsArray.push(postObj);
+      if (popstCount === processed) {
+        writeJson(postsArray);
+      }
+    });
+  });
+
+writeJson = postsarr => {
+  const arr = JSON.stringify(postsarr);
+  const jsonContent = arr;
+
+  fs.writeFile(`./posts.json`, jsonContent, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+  });
+};
